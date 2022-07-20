732f61eaead927608052a0a43683130cc0ee6599
Aarti Dhumal <aarti19dhumal@gmail.com>
2022-07-20 16:52:33 +0530

    This is Calculator_Driver.java and final Commit and check

---
+++ src/week1_Basics_CoreJava/day1/HelloBatch2.java
@@ -0,0 +1,8 @@
+package week1_Basics_CoreJava.day1; //starts with lowercase letter...
+
+public class HelloBatch2 {
+    strictfp final static public int main(String... args){
+        System.out.println("Hello batch 2......");
+        return 0;
+    }
+}
